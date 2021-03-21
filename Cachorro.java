f939acaf9904d1dd9539c73b45aa1932a4d8291a
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-21 19:53:16 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,36 @@
+/*
+ * 
+ */
+package estudodirigido3;
+
+
+public class Cachorro 
+{
+ private String cabeca, tronco, patas;
+
+    public String getCabeca() 
+    {
+        return cabeca;
+    }
+    public void setCabeca(String cabeca) 
+    {
+        this.cabeca = cabeca;
+    }
+
+    public String getTronco() 
+    {
+        return tronco;
+    }
+    public void setTronco(String tronco) 
+    {
+        this.tronco = tronco;
+    }
+
+    public String getPatas() {
+        return patas;
+    }
+    public void setPatas(String patas) {
+        this.patas = patas;
+    }
+    
+}
