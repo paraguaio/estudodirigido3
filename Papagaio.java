9879f729ff745d838a54f87c575ceae70f93338c
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-21 19:54:00 -0300

    Create Papagaio.java

---
+++ Papagaio.java
@@ -0,0 +1,34 @@
+/*
+ * 
+ */
+package estudodirigido3;
+
+
+public class Papagaio 
+{
+    private String cabeca, tronco, patas;
+
+    public String getCabeca() 
+    {
+        return cabeca;
+    }
+    public void setCabeca(String cabeca) 
+    {
+        this.cabeca = cabeca;
+    }
+    public String getTronco() 
+    {
+        return tronco;
+    }
+    public void setTronco(String tronco) 
+    {
+        this.tronco = tronco;
+    }
+
+    public String getPatas() {
+        return patas;
+    }
+    public void setPatas(String patas) {
+        this.patas = patas;
+    }
+}
