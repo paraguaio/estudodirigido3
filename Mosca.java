1dffc7397b5f0956f5f30312d721ccad04f0d85d
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-21 19:53:36 -0300

    Create Mosca.java

---
+++ Mosca.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ */
+package estudodirigido3;
+
+
+public class Mosca 
+{
+    private String cabeca, tronco, patas;
+
+    public String getCabeca() {
+        return cabeca;
+    }
+    public void setCabeca(String cabeca) {
+        this.cabeca = cabeca;
+    }
+
+    public String getTronco() {
+        return tronco;
+    }
+    public void setTronco(String tronco) {
+        this.tronco = tronco;
+    }
+
+    public String getPatas() {
+        return patas;
+    }
+    public void setPatas(String patas) {
+        this.patas = patas;
+    }
+
+    
+}
